9e455b52490c0ee9ec6fcd80f117ec8eed0d3657
Millambeur <florian.chassot@gmail.com>
2014-10-13 14:42:19 +0200

    Create utilitaires.php
    fichier de stockage de fonctions utiles.
    Ajout de la fonction securite_bdd qui traite un string à insérer dans une requête pour éviter une injection.

---
+++ site_FT/PHP/traitement/utilitaires.php
@@ -0,0 +1,16 @@
+<?php
+
+/* fonction qui sécurise $string pour éviter les injections */
+function securite_bdd($string)
+{
+  if (ctype_digit($string)) {
+    $string = intval($string);
+  } else {
+    $string = mysql_real_escape_string($string);
+    $string = addcslashes($string, '%_');
+  }
+        
+  return $string;
+}
+
+?>
