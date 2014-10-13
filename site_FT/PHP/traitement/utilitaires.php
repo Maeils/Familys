1417294fbf895ae2d301883363955e33161ff49b
Millambeur <florian.chassot@gmail.com>
2014-10-13 16:08:09 +0200

    Update utilitaires.php
    Ajout de la fonction get_value_parameter_url qui récupère la valeur du paramètre $parameter dans l'url $url en vérifiant si le fichier correspondant existe bien.

--- site_FT/PHP/traitement/utilitaires.php
+++ site_FT/PHP/traitement/utilitaires.php
@@ -19,4 +19,36 @@ function url_page($page)
   return 'http://www.familysteam.com/index.php?page=' + $page;
 }
 
+/* fonction qui retourne la valeur d'un paramètre dans l'url si le paramètre existe */
+function get_value_parameter_url($url, $parameter)
+{
+  
+	$url = explode('index.php',$url);
+	
+	if (isset($url[1])) {
+	  
+		$tmp = explode('?',$url[1]);
+		$tmp = explode('&', $tmp[1]);
+		
+		/* on cherche si l'un des paramètres de l'url est "page" */
+		$page = explode('=', $tmp[1]);
+		$testpage = $page[0];
+		$max = sizeof($tmp);
+		
+		for ($i = 0; $i < $max && $testpage != 'page'; ++$i) {
+			$page = explode('=', $tmp[$i + 1]);
+			$testpage = $page[0];
+		}
+		
+		if ($testpage == 'page' AND file_exists($page[1].'.php')) {
+		  return $page[1];
+		} else {
+  		return '';
+		}
+	}
+	else{
+		return '';
+	}
+}
+
 ?>
