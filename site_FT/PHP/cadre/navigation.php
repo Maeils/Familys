c9bbbe08eab44beef9fe92408e15127f728c513b
Millambeur <florian.chassot@gmail.com>
2014-10-13 15:04:29 +0200

    Update navigation.php
    v1.0 du menu haut pour une base de tests.

---
+++ site_FT/PHP/cadre/navigation.php
@@ -0,0 +1,27 @@
+<ul class="main-nav">
+  <a href=<?php echo '\"'+url_page('accueil')+'\"' ?> class="lien_menu_haut">
+    <li class="item">
+      <label>Home</label>
+    </li>
+  </a>
+  <a href=<?php echo '\"'+url_page('news')+'\"' ?> class="lien_menu_haut">
+    <li class="item">
+      <label>News</label>
+    </li>
+  </a>
+  <a href=<?php echo '\"'+url_page('gaming')+'\"' ?> class="lien_menu_haut">
+    <li class="item">
+      <label>Gaming</label>
+    </li>
+  </a>
+  <a href=<?php echo '\"'+url_page('familysteam')+'\"' ?> class="lien_menu_haut">
+    <li class="item">
+      <label>La Family's Team</label>
+    </li>
+  </a>
+  <a href=<?php echo '\"'+url_page('contact')+'\"' ?> class="lien_menu_haut">
+    <li class="item">
+      <label>Contact</label>
+    </li>
+  </a>
+</ul>
