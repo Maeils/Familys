9c3a1b24ec569706fdbc4b8981ceee9f5068a25a
Millambeur <florian.chassot@gmail.com>
2014-10-15 19:59:40 +0200

    Silvio jte baise Ajout de la bannière bizarre et trop grande

--- site_FT/PHP/cadre/navigation.php
+++ site_FT/PHP/cadre/navigation.php
@@ -1,26 +1,26 @@
-<div i="div_navigation">
+<div id="div_navigation">
   <ul class="main-nav" id="navigation">
-    <li class="item">
+    <li class="nav_item">
       <a href=<?php echo url_page('accueil'); ?> class="lien_menu_haut">
         <label>Home</label>
       </a>
     </li>
-    <li class="item">
+    <li class="nav_item">
       <a href=<?php echo url_page('insideft'); ?> class="lien_menu_haut">
         <label>News</label>
       </a>
     </li>
-    <li class="item">
+    <li class="nav_item">
       <a href=<?php echo url_page('gaming'); ?> class="lien_menu_haut">
         <label>Gaming</label>
       </a>
     </li>
-    <li class="item">
+    <li class="nav_item">
       <a href=<?php echo url_page('historique'); ?> class="lien_menu_haut">
-        <label>La Family's Team</label>
+        <label>Family</label>
       </a>
     </li>
-    <li class="item">
+    <li class="nav_item">
       <a href=<?php echo url_page('contact'); ?> class="lien_menu_haut">
         <label>Contact</label>
       </a>
