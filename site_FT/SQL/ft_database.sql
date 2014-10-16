ff5f7bff2518b68cbb7ff01c637a9434412eb9c0
Millambeur <florian.chassot@gmail.com>
2014-10-16 03:12:23 +0200

    Failli oublier la bdd avec news =)

--- site_FT/SQL/ft_database.sql
+++ site_FT/SQL/ft_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 15 Octobre 2014 à 14:51
+-- Généré le :  Jeu 16 Octobre 2014 à 03:10
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -40,6 +40,32 @@ CREATE TABLE IF NOT EXISTS `jeu` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `id_news` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `synopsis` varchar(500) NOT NULL,
+  `redacteur` int(11) NOT NULL,
+  PRIMARY KEY (`id_news`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `news`
+--
+
+INSERT INTO `news` (`id_news`, `titre`, `image`, `synopsis`, `redacteur`) VALUES
+(1, 'Ceci est le titre de la news 1 donc tranquille pète un coup', 'news1.jpg', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', 1),
+(2, 'Ceci est le titre de la news 2 donc tranquille pète un coup', 'news2.jpg', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', 1),
+(3, 'Ceci est le titre de la news 3 donc tranquille pète un coup', 'news3.png', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', 1),
+(4, 'Ceci est le titre de la news 4 donc tranquille pète un coup', 'news4.jpg', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', 1),
+(5, 'Ceci est le titre de la news 5 donc tranquille pète un coup', 'news5.jpg', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `personnage`
 --
 
@@ -63,6 +89,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `pseudo` varchar(255) NOT NULL,
+  `motdepasse` varchar(20) NOT NULL,
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
@@ -70,8 +97,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id_user`, `mail`, `nom`, `prenom`, `pseudo`) VALUES
-(1, 'user@ft.fr', 'UserNom', 'UserPrenom', 'Pseudo');
+INSERT INTO `user` (`id_user`, `mail`, `nom`, `prenom`, `pseudo`, `motdepasse`) VALUES
+(1, 'florian.chassot@gmail.com', 'Chassot', 'Florian', 'Millambeur', '123456');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
